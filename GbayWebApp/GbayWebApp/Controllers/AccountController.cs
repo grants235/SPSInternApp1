c7702e5cdbaa33a2c20a344f8eea01d3e16b37bb
Grant <grantshanklin@gmail.com>
2020-05-06 11:00:51 -0500

    Added login function

--- GbayWebApp/GbayWebApp/Controllers/AccountController.cs
+++ GbayWebApp/GbayWebApp/Controllers/AccountController.cs
@@ -86,8 +86,30 @@ namespace GbayWebApp.Controllers
             }
 
         }
-        
 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attepmt");
+                
+            }
+
+            return View(model);
+        }
     }
 }
