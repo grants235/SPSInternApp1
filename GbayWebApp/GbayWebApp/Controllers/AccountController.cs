8e523af808fa71661732ad69ec4fab7ec5abf022
Grant <grantshanklin@gmail.com>
2020-05-06 09:26:15 -0500

    Logout function and navbar style

--- GbayWebApp/GbayWebApp/Controllers/AccountController.cs
+++ GbayWebApp/GbayWebApp/Controllers/AccountController.cs
@@ -20,6 +20,13 @@ namespace GbayWebApp.Controllers
             this.userManager = userManager;
             this.signInManager = signInManager;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
+        }
         
         [HttpGet]
         public IActionResult Register()
