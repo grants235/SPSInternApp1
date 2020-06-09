3b225a33d6598135c796ef66a2e1ede78a70edd2
Grant <grantshanklin@gmail.com>
2020-06-08 21:46:12 -0500

    Revert "Initial Create"
    This reverts commit 65cb6f4ea00074deefb8aa14d9d1212775c36bed.

--- GbayWebApp/GbayWebApp/Controllers/AccountController.cs
+++ GbayWebApp/GbayWebApp/Controllers/AccountController.cs
@@ -188,7 +188,7 @@ namespace GbayWebApp.Controllers
                 {
                     if (user.Email != model.Email)
                     {
-                        ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+                        ModelState.AddModelError(string.Empty, "Invalid Login Attepmt");
                         return View(model);
                     }
 
@@ -273,7 +273,7 @@ namespace GbayWebApp.Controllers
             }
 
             return View();
-            
+           
         }
 
         [HttpGet]
@@ -293,8 +293,6 @@ namespace GbayWebApp.Controllers
                     var token = await userManager.GeneratePasswordResetTokenAsync(user);
 
                     var passwordResetLink = Url.Action("ResetPassword", "Account", new { email = model.Email, token = token }, Request.Scheme);
-
-                    ViewBag.Link = token;
 
                     SmtpClient client = new SmtpClient();
                     client.Connect("smtp.gmail.com", 465, true);
